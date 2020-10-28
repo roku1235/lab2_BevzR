2d8ac634b75a459a7d55d510cceb913917960160
roku1235 <roku1235@mail.ru>
2020-10-28 20:30:46 +0300

    Initial commit

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Лев Николаевич", "Войнаимир@mail.ru", 'М');
+        System.out.println(a1);
+    }
+}
